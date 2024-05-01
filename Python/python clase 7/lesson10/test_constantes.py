46c23b4c34714d0ecbfa9f615de3fc251d9dd374
Maria Elena Haydee Ponce <120025349+hechizera10@users.noreply.github.com>
2024-05-01 13:35:40 -0300

    Python-Semana4

---
+++ Python/python clase 7/lesson10/test_constantes.py
@@ -0,0 +1,10 @@
+from Constantes import *
+from Constantes import Matematicas as Mate
+
+print(MI_CONSTANTE)
+
+#MI_CONSTANTE = "Nuevo valor de mi constante"
+#print(MI_CONSTANTE)
+
+print(Matematicas)
+print(Mate)
